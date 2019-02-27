546da7f4a3b81ba66dc7b84d552a6696de724ca4
HDX180 <489552005@qq.com>
2019-02-27 11:16:23 +0800

    Add files via upload
    add test

---
+++ src/DMS/dmsBench_test.go
@@ -0,0 +1,24 @@
+package dmstest
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+func BenchmarkHealth(b *testing.B) {
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		resp, err := http.Get("http://127.0.0.1:4637/getDevTemp?codeID=12370")
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			// handle error
+		}
+
+		fmt.Println(string(body))
+	}
+
+}
